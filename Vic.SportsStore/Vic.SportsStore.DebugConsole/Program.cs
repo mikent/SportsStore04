b2735a745258b0d3d3447ed5fe43fa7787a3476b
mike <mike>
2019-09-07 18:01:17 -0400

    update

--- Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
+++ Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.DebugConsole
 {
@@ -10,6 +12,22 @@ namespace Vic.SportsStore.DebugConsole
 	{
 		static void Main(string[] args)
 		{
+			using (var ctx = new EFDbContext())
+			{
+				var product = new Product() {
+					Name = "apple1",
+					Price = 1.2m,
+					Description = "fruit",
+					Category = "food",
+				};
+				var c = ctx.Products.Add(product);
+				ctx.SaveChanges();
+				c.Name = "small apple";
+				ctx.SaveChanges();
+			}
+
+			Console.WriteLine("ok");
+			Console.ReadKey();
 		}
 	}
 }
