e97b95e941cc1996d4b0b688319fd6cc0b83adcb
mike <mike>
2019-09-14 15:53:34 -0400

    update

--- Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
@@ -12,8 +12,12 @@ namespace Vic.SportsStore.WebApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.MapRoute(
+				name: null, 
+				url: "Page{page}", 
+				defaults: new { Controller = "Product", action = "List" });
 
-            routes.MapRoute(
+			routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
